4f198b7478543e9c9d96bf4f0429c266015e8040
siara-cc <arun@siara.cc>
2016-01-28 09:18:51 +0530

    wip

--- src/linex.cpp
+++ src/linex.cpp
@@ -2,21 +2,22 @@
 
 void linex::put(char *key, int key_len, char *value, int value_len) {
     // isPut = true;
+    int lastSearchPos[numLevels];
     if (root->filledSize() == 0) {
-        root->addData(key, key_len, value, value_len);
+        root->addData(0, key, key_len, value, value_len, lastSearchPos, 0);
         total_size++;
     } else {
-        linex_block *foundNode;
-        int lastSearchPos[numLevels];
-        int pos = recursiveSearch(key, key_len, root, foundNode, lastSearchPos);
+        int pos = 0;
+        linex_block *foundNode = recursiveSearch(key, key_len, root,
+                lastSearchPos, &pos);
         recursiveUpdate(foundNode, pos, key, key_len, value, value_len,
                 lastSearchPos, numLevels - 1);
     }
     // isPut = false;
 }
 
-int linex::recursiveSearch(char *key, int key_len, linex_block *node,
-        linex_block *foundNode, int lastSearchPos[]) {
+linex_block *linex::recursiveSearch(char *key, int key_len, linex_block *node,
+        int lastSearchPos[], int *pIdx) {
     int level = 0;
     while (!node->isLeaf()) {
         lastSearchPos[level] = node->binarySearch(key, key_len);
@@ -30,96 +31,106 @@ int linex::recursiveSearch(char *key, int key_len, linex_block *node,
                     level++;
                     lastSearchPos[level] = 0;
                 } while (!node->isLeaf());
-                foundNode = node;
-                return lastSearchPos[level];
+                *pIdx = lastSearchPos[level];
+                return node;
             }
         }
         node = node->getChild(lastSearchPos[level]);
         level++;
     }
-    foundNode = node;
-    return node->binarySearch(key, key_len);
+    *pIdx = node->binarySearch(key, key_len);
+    return node;
 }
 
 int linex::get(char *key, int key_len, char *value) {
-    linex_block *foundNode;
     int lastSearchPos[numLevels];
-    int pos = recursiveSearch(key, key_len, root, foundNode, lastSearchPos);
+    int pos = -1;
+    linex_block *foundNode = recursiveSearch(key, key_len, root, lastSearchPos,
+            &pos);
     if (pos < 0)
         return 0;
     return foundNode->getData(pos, value);
 }
 
-void linex::recursiveUpdate(linex_block *node, int pos, char *key, int key_len,
-        char *value, int value_len, int lastSearchPos[], int level) {
-    int idx = lastSearchPos[level];
-    if (idx < 0) {
-        idx = ~idx;
-        if (node->isFull(key_len + value_len)) {
-            linex_block *newNode = new linex_block();
-            Object[] newArray;
-            if (node->isLeaf) {
-                newnode->data = new Comparable[mSize];
-                newnode->first = (Comparable) arr[mSizeBy4];
-                newArray = newnode->data;
+linex_block *linex_block::addData(int *pIdx, char *key, int key_len,
+        char *value, int value_len, int lastInsertPos[], int level) {
+    if (isFull(key_len + value_len)) {
+        linex_block *newnode = new linex_block();
+        if (!isLeaf())
+            newnode->setLeaf(false);
+        int halfKVLen = BLK_SIZE - block_data.hdr.kv_last_pos + 1;
+        halfKVLen /= 2;
+        int kvLen = 0;
+        int *kvIdx = sizeof(block_data.hdr);
+        for (int i = filledSize() - 1; i >= 0; i++) {
+            int keyLen = block_data.buf[kvIdx[i]];
+            keyLen++;
+            int valueLen = block_data.buf[kvIdx[i] + keyLen];
+            valueLen++;
+            kvLen += keyLen;
+            kvLen += valueLen;
+            if (kvLen > halfKVLen) {
+                newnode->block_data.hdr.filledSize = filledSize() - i;
+                break;
             } else {
-                newnode->children = new Node[mSizeBy2];
-                newnode->first = (Comparable)((Node) arr[mSizeBy4]).first;
-                newnode->isLeaf = false;
-                newArray = newnode->children;
-            }
-            newnode->parent = node->parent;
-            System.arraycopy(arr, mSizeBy4, newArray, 0, mSizeBy4);
-            if (node->isLeaf)
-                System.arraycopy(arr, mSizeBy2Plus4, newArray, mSizeBy2,
-                        mSizeBy4);
-            else {
-                for (int i = 0; i < mSizeBy4; i++) {
-                    Node child = (Node) newArray[i];
-                    child.parent = newNode;
-                }
-            }
-            newnode->filledSize() = node->filledSize() = mSizeBy4Minus1;
-            if (node->parent == null) {
-                node->parent = new linex_block();
-                root = node->parent;
-                root.first = node->first;
-                root.children = new Node[mSizeBy2];
-                root.children[0] = node;
-                root.children[1] = newNode;
-                root.isLeaf = false;
-                root.filledSize() = 1;
-                root.lastSearchPos = 0;
-                newnode->parent = root;
-            } else {
-                Node parent = node->parent;
-                if (parent.lastSearchPos == parent.filledSize())
-                    parent.lastSearchPos = ~(parent.filledSize() + 1);
-                else
-                    parent.lastSearchPos = ~(parent.lastSearchPos + 1);
-                recursiveUpdate(parent, newNode, null);
-            }
-            // System.out.println("Grow");
-            if (idx > mSizeBy4Minus1) {
-                node = newNode;
-                arr = (node->isLeaf ? newnode->data : newnode->children);
-                idx -= mSizeBy4;
-                if (!node->isLeaf)
-                    ((Node) key).parent = newNode;
+                memcpy(
+                        newnode->block_data.buf
+                                + newnode->block_data.hdr.kv_last_pos - kvLen,
+                        block_data.buf + kvIdx[i], kvLen);
+                block_data.buf[kvIdx[i]] = ~block_data.buf[kvIdx[i]];
+                newnode->block_data.hdr.kv_last_pos -= kvLen;
             }
         }
-        int idxPlus1 = idx + 1;
-        int idxPlusSizeBy2 = idx + mSizeBy2;
-        if (idx <= node->filledSize())
-            System.arraycopy(arr, idx, arr, idxPlus1,
-                    node->filledSize() - idx + 1);
-        arr[idx] = key;
-        if (node->isLeaf) {
-            if (idx <= node->filledSize())
-                System.arraycopy(arr, idxPlusSizeBy2, arr, idxPlusSizeBy2 + 1,
-                        node->filledSize() - idx + 1);
-            arr[idxPlusSizeBy2] = value;
-            size++;
+        int newLen = 0;
+        for (int i = block_data.hdr.kv_last_pos; i < BLK_SIZE;) {
+            int keyLen = block_data.buf[i];
+            int kvLen = 0;
+            if (keyLen < 0) {
+                keyLen = ~keyLen;
+                kvLen = keyLen;
+                kvLen++;
+                kvLen += block_data.buf[i];
+                kvLen++;
+                memmove(block_data.buf + i, block_data.buf + i + kvLen, kvLen);
+            } else {
+                kvLen = keyLen;
+                kvLen++;
+                kvLen += block_data.buf[i];
+                kvLen++;
+                newLen += kvLen;
+            }
+            i += kvLen;
+        }
+        byte *kv_last_idx = block_data.buf + block_data.hdr.kv_last_pos;
+        memmove(kv_last_idx + newLen, kv_last_idx, newLen);
+        block_data.hdr.kv_last_pos += newLen;
+        if (node->getParent() == null) {
+            root = new linex_block();
+            node->setParent(root);
+            root = node->parent;
+            root.first = node->first;
+            root.children = new Node[mSizeBy2];
+            root.children[0] = node;
+            root.children[1] = newNode;
+            root.isLeaf = false;
+            root.filledSize() = 1;
+            root.lastSearchPos = 0;
+            newnode->parent = root;
+        } else {
+            Node parent = node->parent;
+            if (parent.lastSearchPos == parent.filledSize())
+                parent.lastSearchPos = ~(parent.filledSize() + 1);
+            else
+                parent.lastSearchPos = ~(parent.lastSearchPos + 1);
+            recursiveUpdate(parent, newNode, null);
+        }
+        // System.out.println("Grow");
+        if (idx > mSizeBy4Minus1) {
+            node = newNode;
+            arr = (node->isLeaf ? newnode->data : newnode->children);
+            idx -= mSizeBy4;
+            if (!node->isLeaf)
+                ((Node) key).parent = newNode;
         }
         if (idx == 0) {
             if (node->isLeaf)
@@ -128,8 +139,37 @@ void linex::recursiveUpdate(linex_block *node, int pos, char *key, int key_len,
                 node->first = (Comparable)((Node) arr[0]).first;
             updateParentsRecursively(node, node->first);
         }
-        node->filledSize()++;}
-else    {
+    }
+    int idxPlus1 = *pIdx + 1;
+    if (idx <= node->filledSize())
+        System.arraycopy(arr, idx, arr, idxPlus1, node->filledSize() - idx + 1);
+    arr[idx] = key;
+    if (node->isLeaf) {
+        if (idx <= node->filledSize())
+            System.arraycopy(arr, idxPlusSizeBy2, arr, idxPlusSizeBy2 + 1,
+                    node->filledSize() - idx + 1);
+        arr[idxPlusSizeBy2] = value;
+        size++;
+    }
+    node->filledSize()++;}
+
+void linex_block::updateParentsRecursively(Node node, Comparable first) {
+    if (node == null)
+        return;
+    Node parent = node->parent;
+    if (parent != null && parent.children[0].equals(node)) {
+        parent.first = first;
+        updateParentsRecursively(parent, first);
+    }
+}
+
+void linex::recursiveUpdate(linex_block *node, int pos, char *key, int key_len,
+        char *value, int value_len, int lastSearchPos[], int level) {
+    int idx = lastSearchPos[level];
+    if (idx < 0) {
+        idx = ~idx;
+        node = node->addData(&idx, key, key_len, value, value_len);
+    } else {
         if (node->isLeaf) {
             int vIdx = idx + mSizeBy2;
             returnValue = (V) arr[vIdx];
@@ -139,77 +179,43 @@ else    {
     return returnValue;
 }
 
-private void updateParentsRecursively(Node node, Comparable first) {
-    if (node == null)
-    return;
-    Node parent = node->parent;
-    if (parent != null && parent.children[0].equals(node)) {
-        parent.first = first;
-        updateParentsRecursively(parent, first);
-    }
-}
-
-static int[] roots;
-static int[] left;
-static int[] ryte;
-static int[] parent;
-static byte[] bit_count = new byte[256];
-static {
-    if (left == null) {
-        GenTree.TREE_SIZE = mSizeBy2;
-        GenTree.generateLists();
-        left = new int[GenTree.TREE_SIZE];
-        ryte = new int[GenTree.TREE_SIZE];
-        roots = new int[GenTree.TREE_SIZE];
-        for (int i = 0; i < GenTree.TREE_SIZE; i++) {
-            left[i] = GenTree.lstLeft.get(i);
-            ryte[i] = GenTree.lstRyte.get(i);
-            roots[i] = GenTree.lstRoots.get(i);
-        }
-    }
-    for (int i = 0; i < 256; i++) {
-        bit_count[i] = countSetBits(i);
-    }
-}
-
 int linex_block::binarySearchLeaf(Object key, int key_len) {
     int middle, cmp, filledSize();
     filledSize() = node->filledSize();
-    middle = roots[filledSize()];
+    middle = GenTree::roots[filledSize()];
     do {
         cmp = node->data[middle].compareTo(key);
         if (cmp < 0) {
-            middle = ryte[middle];
+            middle = GenTree::ryte[middle];
             while (middle > filledSize())
-                middle = left[middle];
+                middle = GenTree::left[middle];
         } else if (cmp > 0)
-            middle = left[middle];
+            middle = GenTree::left[middle];
         else
             return middle;
     } while (middle >= 0);
     return middle;
 }
 
-int linex_block::binarySearchNode(Object key, int key_len) {
-    int middle, cmp, filledSize();
-    filledSize() = node->filledSize();
-    middle = roots[filledSize()];
+int linex_block::binarySearchNode(char *key, int key_len) {
+    int middle, cmp;
+    middle = GenTree::roots[filledSize];
     do {
-        Node child = node->children[middle];
-        cmp = child.first.compareTo(key);
+        char *key_middle = getKey[middle];
+        cmp = mem.compareTo(key);
         if (cmp > 0)
-            middle = left[middle];
+            middle = GenTree::left[middle];
         else if (cmp < 0) {
             Node nextChild = null;
-            if (node->filledSize() > middle) {
+            if (filledSize > middle) {
                 nextChild = node->children[middle + 1];
                 cmp = nextChild.first.compareTo(key);
                 if (cmp > 0)
                     return middle;
                 else if (cmp < 0) {
-                    middle = ryte[middle];
+                    middle = GenTree::ryte[middle];
                     while (middle > filledSize())
-                        middle = left[middle];
+                        middle = GenTree::left[middle];
                 } else
                     return ~(middle + 2 + filledSize());
             } else
@@ -231,6 +237,7 @@ long linex::size() {
 }
 
 linex::linex() {
+    GenTree::generateLists();
     root = new linex_block();
     total_size = 0;
     numLevels = 1;
@@ -241,11 +248,14 @@ linex_block::linex_block() {
     block_data.hdr.isLeaf = 1;
     block_data.hdr.filledSize = 0;
     block_data.hdr.kv_last_pos = sizeof(block_data.buf);
-    block_data.hdr.parent = null;
 }
 
 bool linex_block::isLeaf() {
     return (block_data.hdr.isLeaf == 1);
+}
+
+void linex_block::setLeaf(byte isLeaf) {
+    block_data.hdr.isLeaf = isLeaf;
 }
 
 bool linex_block::isFull(int kv_len) {
@@ -263,9 +273,6 @@ int linex_block::filledSize() {
 }
 
 linex_block *linex_block::getChild(int pos) {
-}
-
-void linex_block::addData(char *key, int key_len, char *value, int value_len) {
 }
 
 int linex_block::getData(int pos, char *data) {
--- src/linex.h
+++ src/linex.h
@@ -4,6 +4,9 @@
 typedef unsigned char byte;
 #define null 0
 #define BLK_SIZE 512
+#define TREE_SIZE 128
+#define MAX_DATA_LEN 127
+
 class linex_block {
 private:
     union {
@@ -12,19 +15,23 @@ private:
             byte isLeaf;
             byte filledSize;
             int kv_last_pos;
-            linex_block *parent;
         } hdr;
     } block_data;
     int binarySearchLeaf(char *key, int key_len);
     int binarySearchNode(char *key, int key_len);
+    void updateParentsRecursively(Node node, Comparable first);
 public:
     linex_block();
     bool isLeaf();
+    void setLeaf(byte isLeaf);
     bool isFull(int kv_len);
+    linex_block *getParent();
+    void setParent(linex_block *p);
     int filledSize();
     linex_block *getChild(int pos);
     int binarySearch(char *key, int key_len);
-    void addData(char *key, int key_len, char *value, int value_len);
+    linex_block *addData(int *pIdx, char *key, int key_len, char *value,
+            int value_len, int lastSearchPos[], int level);
     int getData(int pos, char *data);
 };
 
@@ -33,8 +40,8 @@ private:
     linex_block *root;
     long total_size;
     int numLevels;
-    int recursiveSearch(char *key, int key_len, linex_block *node,
-            linex_block *foundNode, int lastSearchPos[]);
+    linex_block *recursiveSearch(char *key, int key_len, linex_block *node,
+            int lastSearchPos[], int *pIdx);
     void recursiveUpdate(linex_block *foundNode, int pos, char *key,
             int key_len, char *value, int value_len, int lastSearchPos[],
             int level);
@@ -43,6 +50,100 @@ public:
     long size();
     int get(char *key, int key_len, char *value);
     void put(char *key, int key_len, char *value, int value_len);
+};
+
+class GenTree {
+public:
+    static byte bit_count[256];
+    static int *roots;
+    static int *left;
+    static int *ryte;
+    static int *parent;
+    static int ixRoots;
+    static int ixLeft;
+    static int ixRyte;
+    static int ixPrnt;
+
+    static int binarySearch(int idx) {
+        int first, last;
+        int middle, prevMiddle;
+        last = TREE_SIZE - 1;
+        first = 0;
+        middle = (last - first) / 2;
+        prevMiddle = middle;
+        while (first <= last) {
+            if (idx == middle) {
+                int oldLast = last;
+                parent[ixPrnt++] = prevMiddle;
+                last = middle - 1;
+                if (first <= last)
+                    left[ixLeft++] = (first + (last - first) / 2);
+                else
+                    left[ixLeft++] = ~idx;
+                last = oldLast;
+                first = middle + 1;
+                if (first <= last)
+                    ryte[ixRyte++] = (first + (last - first) / 2);
+                else
+                    ryte[ixRyte++] = ~(idx + 1);
+                return middle;
+            }
+            if (idx > middle)
+                first = middle + 1;
+            else
+                last = middle - 1;
+            prevMiddle = middle;
+            middle = first + (last - first) / 2;
+        }
+        return ~middle;
+    }
+
+    static void generateLists() {
+        ixRoots = 0;
+        ixLeft = 0;
+        ixRyte = 0;
+        ixPrnt = 0;
+        roots = new int[TREE_SIZE];
+        left = new int[TREE_SIZE];
+        ryte = new int[TREE_SIZE];
+        parent = new int[TREE_SIZE];
+        int j = 0;
+        int nxt = 1;
+        for (int i = 0; i < TREE_SIZE; i++) {
+            if (i == 0)
+                roots[ixRoots++] = 0;
+            else {
+                int root = 2 ^ nxt;
+                root--;
+                if (i + 1 == TREE_SIZE) {
+                    roots[ixRoots] = roots[ixRoots - 1];
+                    ixRoots++;
+                } else
+                    roots[ixRoots++] = root;
+                j++;
+                if (j > root) {
+                    j = 0;
+                    nxt++;
+                }
+            }
+            binarySearch(i);
+        }
+        for (int i = 0; i < 256; i++) {
+            bit_count[i] = countSetBits(i);
+        }
+    }
+
+    // Function to get no of set bits in binary
+    // representation of passed binary no.
+    static byte countSetBits(int n) {
+        int count = 0;
+        while (n > 0) {
+            n &= (n - 1);
+            count++;
+        }
+        return (byte) count;
+    }
+
 };
 
 #endif
