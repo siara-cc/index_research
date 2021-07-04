108a9249c9d73d0a2652f782180ee076fbd8cb9c
Arun <arun@siara.cc>
2021-07-04 20:04:58 +0530

    Fix compile.sh

--- compile.sh
+++ compile.sh
@@ -2,6 +2,7 @@ g++ -I./hdr -I./src -O3 -Wall -c -fmessage-length=0 -o Release/src/huge_db.o ./s
 g++ -I./hdr -I./src -O3 -Wall -c -fmessage-length=0 -o Release/src/art.o ./src/art.cpp
 g++ -I./hdr -I./src -O3 -Wall -c -fmessage-length=0 -o Release/src/rb_tree.o ./src/rb_tree.cpp
 g++ -I./hdr -I./src -O3 -Wall -c -fmessage-length=0 -o Release/src/dfox.o ./src/dfox.cpp
+g++ -I./hdr -I./src -O3 -Wall -c -fmessage-length=0 -o Release/src/dfos.o ./src/dfos.cpp
 g++ -I./hdr -I./src -O3 -Wall -c -fmessage-length=0 -o Release/src/dfqx.o ./src/dfqx.cpp
 g++ -I./hdr -I./src -O3 -Wall -c -fmessage-length=0 -o Release/src/bfos.o ./src/bfos.cpp
 g++ -I./hdr -I./src -O3 -Wall -c -fmessage-length=0 -o Release/src/bft.o ./src/bft.cpp
@@ -9,6 +10,6 @@ g++ -I./hdr -I./src -O3 -Wall -c -fmessage-length=0 -o Release/src/dft.o ./src/d
 g++ -I./hdr -I./src -O3 -Wall -c -fmessage-length=0 -o Release/src/GenTree.o ./src/GenTree.cpp
 g++ -I./hdr -I./src -O3 -Wall -c -fmessage-length=0 -o Release/src/linex.o ./src/linex.cpp
 g++ -I./hdr -I./src -O3 -Wall -c -fmessage-length=0 -o Release/src/basix.o ./src/basix.cpp
-g++ -I./hdr -I./src -O3 -Wall -c -fmessage-length=0 -o src/univix_util.o ./src/univix_util.cpp
+g++ -I./hdr -I./src -O3 -Wall -c -fmessage-length=0 -o Release/src/univix_util.o ./src/univix_util.cpp
 cd Release
-g++ -o ../huge_db src/rb_tree.o src/basix.o src/huge_db.o src/dfox.o src/dfqx.o src/linex.o src/bfos.o src/dft.o src/bft.o src/art.o src/GenTree.o src/univix_util.o
+g++ -o ../huge_db src/rb_tree.o src/basix.o src/huge_db.o src/dfox.o src/dfos.o src/dfqx.o src/linex.o src/bfos.o src/dft.o src/bft.o src/art.o src/GenTree.o src/univix_util.o
