352b91ba3766fa2e42db8dacfde0c3363dbd925a
Arun <arun@siara.cc>
2022-10-09 12:15:47 +0530

    Choice FOPEN/OPEN for lru_cache

--- src/lru_cache.h
+++ src/lru_cache.h
@@ -17,6 +17,8 @@
 #include <stdlib.h>
 #include <errno.h>
 
+#define USE_FOPEN 1
+
 typedef unsigned char byte;
 
 using namespace std;
@@ -48,7 +50,11 @@ protected:
     dbl_lnklst *llarr;
     set<int> new_pages;
     const char *filename;
+#if USE_FOPEN == 1
+    FILE *fp;
+#else
     int fd;
+#endif
     size_t file_page_count;
     size_t last_pages_to_flush;
     byte empty;
@@ -57,11 +63,19 @@ protected:
         //if (is_new)
         //  fseek(fp, 0, SEEK_END);
         //else
+#if USE_FOPEN == 1
+        if (fseeko64(fp, file_pos, SEEK_SET))
+            fseeko64(fp, 0, SEEK_END);
+        int write_count = fwrite(block, 1, bytes, fp);
+#else
         if (lseek64(fd, file_pos, SEEK_SET) == -1)
-          lseek64(fd, 0, SEEK_END);
+            lseek64(fd, 0, SEEK_END);
         int write_count = write(fd, block, bytes);
-        if (write_count != bytes)
+#endif
+        if (write_count != bytes) {
+            printf("Short write: %d\n", write_count);
             throw EIO;
+        }
     }
     void write_pages(set<int>& pages_to_write) {
         for (int it : pages_to_write) {
@@ -74,7 +88,9 @@ protected:
         }
     }
     void calc_flush_count() {
-        last_pages_to_flush = file_page_count / cache_size_in_pages * 40;
+        last_pages_to_flush = 10;
+        return;
+        //file_page_count / cache_size_in_pages * 40;
         if (last_pages_to_flush < 10)
             last_pages_to_flush = 10;
         if (last_pages_to_flush > 50)
@@ -128,16 +144,40 @@ public:
         disk_to_cache_map.reserve(page_count);
         skip_page_count = init_page_count;
         file_page_count = init_page_count;
+        struct stat file_stat;
+        memset(&file_stat, '\0', sizeof(file_stat));
+#if USE_FOPEN == 1
+        fp = fopen(fname, "r+b");
+        if (fp == NULL) {
+          fp = fopen(fname, "wb");
+          if (fp == NULL)
+            throw errno;
+          fclose(fp);
+          fp = fopen(fname, "r+b");
+          if (fp == NULL)
+            throw errno;
+        }
+        lstat(fname, &file_stat);
+#else
         fd = open(fname, O_RDWR | O_CREAT | O_LARGEFILE, 0644);
         if (fd == -1)
           throw errno;
-        struct stat file_stat;
         fstat(fd, &file_stat);
+#endif
         file_page_count = file_stat.st_size;
         if (file_page_count > 0)
            file_page_count /= page_size;
         cout << "File page count: " << file_page_count << endl;
         empty = 0;
+#if USE_FOPEN == 1
+        if (fread(root_block, 1, page_size, fp) != page_size) {
+            file_page_count = 1;
+            fseeko64(fp, 0, SEEK_SET);
+            if (fwrite(root_block, 1, page_size, fp) != page_size)
+              throw EIO;
+            empty = 1;
+        }
+#else
         if (read(fd, root_block, page_size) != page_size) {
             file_page_count = 1;
             lseek64(fd, 0, SEEK_SET);
@@ -145,6 +185,7 @@ public:
               throw EIO;
             empty = 1;
         }
+#endif
         memset(&stats, '\0', sizeof(stats));
         calc_flush_count();
     }
@@ -157,11 +198,12 @@ public:
         }
         write_pages(pages_to_write);
         free(page_cache);
-        lseek64(fd, 0, SEEK_SET);
-        int write_count = write(fd, root_block, page_size);
-        if (write_count != page_size)
-            throw EIO;
+        write_page(root_block, 0, page_size);
+#if USE_FOPEN == 1
+        fclose(fp);
+#else
         close(fd);
+#endif
         free(root_block);
         free(llarr);
         cout << "total_cache_misses: " << " " << stats.total_cache_misses << endl;
@@ -237,16 +279,27 @@ public:
             if (!is_new && new_pages.find(disk_page) == new_pages.end()) {
                 off64_t file_pos = page_size;
                 file_pos *= disk_page;
+#if USE_FOPEN == 1
+                if (!fseeko64(fp, file_pos, SEEK_SET)) {
+                    int read_count = fread(&page_cache[page_size * cache_pos], 1, page_size, fp);
+                    //printf("read_count: %d, %d, %d, %d, %ld\n", read_count, page_size, disk_page, (int) page_cache[page_size * cache_pos], ftell(fp));
+                    if (read_count != page_size) {
+                       perror("read");
+                    }
+                } else {
+                  printf("disk_page: %d, %d\n", disk_page, errno);
+                }
+#else
                 if (lseek64(fd, file_pos, SEEK_SET) != -1) {
                     int read_count = read(fd, &page_cache[page_size * cache_pos], page_size);
                     //printf("read_count: %d, %d, %d, %d, %ld\n", read_count, page_size, disk_page, (int) page_cache[page_size * cache_pos], ftell(fp));
                     if (read_count != page_size) {
                        perror("read");
-                    } // ignore if unable to read the page - it may not exist
-                        //cout << "2:Only " << read_count << " bytes read at position: " << page_size * disk_page << endl;
+                    }
                 } else {
                   printf("disk_page: %d, %d\n", disk_page, errno);
                 }
+#endif
             }
         } else {
             dbl_lnklst *current_entry = disk_to_cache_map[disk_page];
