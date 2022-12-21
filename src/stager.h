3f4e1375b4dbe11eebb19d96757edba7af6d9592
Arun <arun@siara.cc>
2022-12-21 16:53:49 +0530

    Add idx2

--- src/stager.h
+++ src/stager.h
@@ -9,26 +9,33 @@ class stager {
     protected:
       basix *idx0;
       basix *idx1;
+      basix *idx2;
       bool is_cache0_full;
 
     public:
         stager(const char *fname, size_t cache_size) {
             char fname0[strlen(fname) + 5];
             char fname1[strlen(fname) + 5];
+            char fname2[strlen(fname) + 5];
             strcpy(fname0, fname);
             strcpy(fname1, fname);
+            strcpy(fname2, fname);
             strcat(fname0, ".ix0");
             strcat(fname1, ".ix1");
+            strcat(fname2, ".ix2");
             int cache0_size = cache_size;
-            int cache1_size = cache_size;
+            int cache1_size = cache_size * 2;
+            int cache2_size = cache_size * 2;
             idx0 = new basix(0, 0, cache0_size, fname0);
             idx1 = new basix(4096, 4096, cache1_size, fname1);
+            idx2 = new basix(4096, 4096, cache2_size, fname2);
             is_cache0_full = false;
         }
 
         ~stager() {
             delete idx0;
             delete idx1;
+            delete idx2;
         }
 
         uint8_t *put(const char *key, uint8_t key_len, const char *value,
@@ -43,6 +50,12 @@ class stager {
                 val = idx1->get(key, key_len, pValueLen);
                 if (val != NULL && idx1->isChanged())
                     return val;
+                uint8_t start_count = (val == NULL ? 1 : 2);
+                if (val == NULL) {
+                    val = idx2->get(key, key_len, pValueLen);
+                    //if (val != NULL && idx2->isChanged())
+                    //    return val;
+                }
                 int new_val_len = (val == NULL ? value_len : *pValueLen);
                 uint8_t new_val[new_val_len + 1];
                 memcpy(new_val, val == NULL ? value : val, new_val_len);
@@ -71,9 +84,9 @@ class stager {
                             if (entry_count <= cur_count) {
                                 if (is_first_iter) {
                                     int16_t v1_len = 0;
-                                    uint8_t *v1 = idx1->get(k, k_len, &v1_len);
-                                    if (v1 != NULL && idx1->isChanged()) {
-                                        v1 = idx1->put(k, k_len, v, v_len - 1, NULL);
+                                    uint8_t *v1 = idx2->get(k, k_len, &v1_len);
+                                    if (v1 != NULL && idx2->isChanged()) {
+                                        v1 = idx2->put(k, k_len, v, v_len - 1, NULL);
                                         if (v1 != NULL)
                                             memcpy(v1, v, v_len - 1);
                                         idx0->remove_entry(i);
@@ -81,7 +94,11 @@ class stager {
                                     }
                                 } else {
                                     int16_t v1_len = 0;
-                                    uint8_t *v1 = idx1->put(k, k_len, v, v_len - 1, &v1_len);
+                                    uint8_t *v1;
+                                    if (entry_count == 1)
+                                        v1 = idx2->put(k, k_len, v, v_len - 1, &v1_len);
+                                    else
+                                        v1 = idx1->put(k, k_len, v, v_len - 1, &v1_len);
                                     if (v1 != NULL)
                                         memcpy(v1, v, v_len - 1);
                                     idx0->remove_entry(i);
@@ -90,7 +107,7 @@ class stager {
                             } else {
                                 if (entry_count < next_min)
                                     next_min = entry_count;
-                                if (entry_count > 0)
+                                if (entry_count > 2)
                                     v[v_len - 1]--;
                             }
                         }
@@ -124,28 +141,32 @@ class stager {
             uint8_t *val = idx0->get(key, key_len, pValueLen);
             if (val == NULL)
                 return idx1->get(key, key_len, pValueLen);
+            if (val == NULL)
+                return idx2->get(key, key_len, pValueLen);
             return val;
         }
 
         cache_stats get_cache_stats() {
-            return idx1->get_cache_stats();
+            return idx2->get_cache_stats();
         }
         int get_max_key_len() {
-            return max(idx0->get_max_key_len(), idx1->get_max_key_len());
+            return max(max(idx0->get_max_key_len(), idx1->get_max_key_len()), idx2->get_max_key_len());
         }
         int getNumLevels() {
-            return idx1->getNumLevels();
+            return idx2->getNumLevels();
         }
         void printStats(long sz) {
             idx0->printStats(idx0->size());
             idx1->printStats(idx1->size());
+            idx2->printStats(idx2->size());
         }
         void printNumLevels() {
             idx0->printNumLevels();
             idx1->printNumLevels();
+            idx2->printNumLevels();
         }
         long size() {
-            return idx0->size() + idx1->size();
+            return idx0->size() + idx1->size() + idx2->size();
         }
 
 };
