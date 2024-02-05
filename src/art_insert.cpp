b720d26ad936018b1465cf5cbdcf521733b90a5c
Arun <arun@siara.cc>
2024-02-05 14:28:27 +0530

    art_insert

--- src/art_insert.cpp
+++ src/art_insert.cpp
@@ -1,7 +1,8 @@
-#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "art.cpp"
 
@@ -22,33 +23,64 @@ int main(int argc, char *argv[]) {
 
   art_tree at;
   art_tree_init(&at);
-  std::vector<std::string> lines;
+  std::vector<uint8_t *> lines;
 
   clock_t t = clock();
-  fstream infile;
-  infile.open(argv[1], ios::in);
-  int line_count = 0;
-  if (infile.is_open()) {
-    string line;
-    string prev_line = "";
-    while (getline(infile, line)) {
-      if (line == prev_line)
-         continue;
-      // sb.append((const uint8_t *) line.c_str(), line.length(), (const uint8_t *) line.c_str(), line.length() > 6 ? 7 : line.length());
-      // sb.append((const uint8_t *) line.c_str(), line.length());
-      //sb.insert((const uint8_t *) line.c_str(), line.length(), (const uint8_t *) line.c_str(), line.length() > 6 ? 7 : line.length());
-      //sb.insert((const uint8_t *) line.c_str(), line.length());
-      lines.push_back(line);
-      art_insert(&at, (const uint8_t *) line.c_str(), line.length(), (void *) lines[line_count].c_str());
-      prev_line = line;
-      line_count++;
-      if ((line_count % 100000) == 0) {
-        cout << ".";
-        cout.flush();
-      }
-    }
-    infile.close();
+
+  struct stat file_stat;
+  memset(&file_stat, '\0', sizeof(file_stat));
+  stat(argv[1], &file_stat);
+  uint8_t *file_buf = (uint8_t *) malloc(file_stat.st_size + 1);
+  printf("File_size: %lld\n", file_stat.st_size);
+
+  FILE *fp = fopen(argv[1], "rb");
+  if (fp == NULL) {
+    perror("Could not open file; ");
+    free(file_buf);
+    return 1;
   }
+  size_t res = fread(file_buf, 1, file_stat.st_size, fp);
+  if (res != file_stat.st_size) {
+    perror("Error reading file: ");
+    free(file_buf);
+    return 1;
+  }
+  fclose(fp);
+
+  int line_count = 0;
+  uint8_t *prev_line = {0};
+  size_t prev_line_len = 0;
+  uint8_t *line = file_buf;
+  uint8_t *cr_pos = (uint8_t *) memchr(line, '\n', file_stat.st_size);
+  if (cr_pos == NULL)
+    cr_pos = file_buf + file_stat.st_size;
+  else {
+    if (cr_pos > line && *(cr_pos - 1) == '\r')
+      cr_pos--;
+  }
+  *cr_pos = 0;
+  size_t line_len = cr_pos - line;
+  do {
+    if (prev_line_len == line_len && strncmp((const char *) line, (const char *) prev_line, prev_line_len) == 0)
+      continue;
+    art_insert(&at, line, line_len, line);
+    lines.push_back(line);
+    prev_line = line;
+    prev_line_len = line_len;
+    line_count++;
+    if ((line_count % 100000) == 0) {
+      cout << ".";
+      cout.flush();
+    }
+    line = cr_pos + (cr_pos[1] == '\n' ? 2 : 1);
+    cr_pos = (uint8_t *) memchr(line, '\n', file_stat.st_size - (cr_pos - file_buf));
+    if (cr_pos != NULL && cr_pos > line) {
+      if (*(cr_pos - 1) == '\r')
+        cr_pos--;
+      *cr_pos = 0;
+      line_len = cr_pos - line;
+    }
+  } while (cr_pos != NULL);
   std::cout << std::endl;
   t = print_time_taken(t, "Time taken for insert/append: ");
 
@@ -56,15 +88,15 @@ int main(int argc, char *argv[]) {
   line_count = 0;
   bool success = false;
   for (int i = 0; i < lines.size(); i++) {
-    std::string& line = lines[i];
-    uint8_t *res_val = (uint8_t *) art_search(&at, (const uint8_t *) line.c_str(), line.length());
+    line = lines[i];
+    line_len = strlen((const char *) line);
+    uint8_t *res_val = (uint8_t *) art_search(&at, line, line_len);
     if (res_val != NULL) {
-      int val_len = line.length() > 6 ? 7 : line.length();
-      if (memcmp(line.c_str(), (const void *) res_val, val_len) != 0) {
-        printf("key: [%.*s], val: [%.*s]\n", (int) line.length(), line.c_str(), val_len, res_val);
+      int val_len = line_len > 6 ? 7 : line_len;
+      if (memcmp(line, res_val, val_len) != 0) {
+        printf("key: [%.*s], val: [%.*s]\n", (int) line_len, line, val_len, res_val);
         err_count++;
       }
-      //free(res_val);
     } else {
       std::cout << line << std::endl;
       err_count++;
@@ -73,5 +105,7 @@ int main(int argc, char *argv[]) {
   }
   t = print_time_taken(t, "Time taken for retrieve: ");
   printf("Lines: %d, Errors: %d\n", line_count, err_count);
+
+  free(file_buf);
 
 }
